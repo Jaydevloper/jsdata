92c7e8c3335a564fe2d51b762faf434c7e245311
Jaydeveloper <jayhanmirx@gmail.com>
2022-05-20 10:22:46 +0500

    vazifa3

--- js/main.js
+++ js/main.js
@@ -99,72 +99,29 @@ const data = [
     ip_address: "177.47.251.168",
   },
 ];
-const img = document.getElementsByClassName("user__img");
-const fname = document.getElementsByClassName("user__fullname");
-const email = document.getElementsByClassName("user__email");
-const gender = document.getElementsByClassName("user__gender");
-const address = document.getElementsByClassName("user__address");
-img[0].src = data[0].src;
-fname[0].textContent = [data[0].first_name + " " + data[0].last_name];
-email[0].textContent = data[0].email;
-gender[0].textContent = data[0].gender;
-address[0].textContent = data[0].ip_address;
+const ul = document.querySelector('ul')
+for(let i = 0;i <= data.length; i++ ) {
+const li = document.createElement('li');
+li.className = ('user card d-inline-block col-12 col-lg-4 col-md-6 col-xl-3 px-2"');
+const div = document.createElement('div');
+div.className =('user__content card-body"');
+const img = document.createElement('img');
+img.className = ('user__img card-img');
+img.src = data[i].src;
+const h3 = document.createElement('h3');
+h3.className = ('user__fullname h5 mt-3');
+h3.textContent = [data[i].first_name + data[i].last_name];
+const a = document.createElement('a');
+a.className = ('user__email h6 text-primary d-block');
+a.textContent =  data[i].email;
+const p = document.createElement('p');
+p.className = ('user__gender h6');
+p.textContent = data[i].gender
+const p2 = document.createElement('p');
+p2.className = ('user__address text-bg-info rounded text-white ps-2"') ;
+p2.textContent = data[i].ip_address;     
+div.append(img,h3,a,p,p2);
+li.append(div); 
+ul.append(li);
 
-img[1].src = data[1].src;
-fname[1].textContent = [data[1].first_name + " " + data[1].last_name];
-email[1].textContent = data[1].email;
-gender[1].textContent = data[1].gender;
-address[1].textContent = data[1].ip_address;
-img[2].src = data[2].src;
-fname[2].textContent = [data[2].first_name + " " + data[2].last_name];
-email[2].textContent = data[2].email;
-gender[2].textContent = data[2].gender;
-address[2].textContent = data[2].ip_address;
-img[3].src = data[3].src;
-fname[3].textContent = [data[3].first_name + " " + data[3].last_name];
-email[3].textContent = data[3].email;
-gender[3].textContent = data[3].gender;
-address[3].textContent = data[3].ip_address;
-img[4].src = data[4].src;
-fname[4].textContent = [data[4].first_name + " " + data[4].last_name];
-email[4].textContent = data[4].email;
-gender[4].textContent = data[4].gender;
-address[4].textContent = data[4].ip_address;
-img[5].src = data[5].src;
-fname[5].textContent = [data[5].first_name + " " + data[5].last_name];
-email[5].textContent = data[5].email;
-gender[5].textContent = data[5].gender;
-address[5].textContent = data[5].ip_address;
-img[6].src = data[6].src;
-fname[6].textContent = [data[6].first_name + " " + data[6].last_name];
-email[6].textContent = data[6].email;
-gender[6].textContent = data[6].gender;
-address[6].textContent = data[6].ip_address;
-img[7].src = data[7].src;
-fname[7].textContent = [data[7].first_name + " " + data[7].last_name];
-email[7].textContent = data[7].email;
-gender[7].textContent = data[7].gender;
-address[7].textContent = data[7].ip_address;
-img[8].src = data[8].src;
-fname[8].textContent = [data[8].first_name + " " + data[8].last_name];
-email[8].textContent = data[8].email;
-gender[8].textContent = data[8].gender;
-address[8].textContent = data[8].ip_address;
-img[9].src = data[9].src;
-fname[9].textContent = [data[9].first_name + " " + data[9].last_name];
-email[9].textContent = data[9].email;
-gender[9].textContent = data[9].gender;
-address[9].textContent = data[9].ip_address;
-img[10].src = data[10].src;
-fname[10].textContent = [data[10].first_name + " " + data[10].last_name];
-email[10].textContent = data[10].email;
-gender[10].textContent = data[10].gender;
-address[10].textContent = data[10].ip_address;
-//2-usul
-// let i;
-// for (i = 0; i <= 11; i++){
-// img[i].src = data[i].src;
-// fname[i].textContent = [data[i].first_name + " " + data[i].last_name];
-// email[i].textContent = data[i].email;
-// gender[i].textContent = data[i].gender;
-// address[i].textContent = data[i].ip_address;
+}
